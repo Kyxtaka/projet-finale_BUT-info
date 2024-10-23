c551ff9a12fb0167edf874d20377d419eecfa696
Clemence <clemence.bcq@gmail.com~>
2024-10-23 16:50:40 +0200

    connexion et inscription

--- mobilist/views.py
+++ mobilist/views.py
@@ -1,12 +1,16 @@
 from .app import app
 from flask import redirect, render_template, url_for
-from flask import redirect, render_template, url_for
 
 @app.route("/")
 def home():
-    return render_template("connexion.html")
+    return render_template("index.html")
 
 
 @app.route("/login")
 def connexion():
     return render_template("connexion.html")
+
+
+@app.route("/inscription")
+def inscription():
+    return render_template("inscription.html")
