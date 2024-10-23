8f68583e5c10e850e783c130beec39e123509d1d
Ryry <yohann.vilcoq@etu.univ-orleans.fr>
2024-10-23 15:32:02 +0200

    correction de la connection a la DB

--- mobilist/app.py
+++ mobilist/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy  import SQLAlchemy
 from flask_bootstrap import Bootstrap5
 from .models import ouvrir_connexion
 
@@ -10,7 +11,11 @@ PORT = 3307
 
 app = Flask( __name__ )
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{USER}:{PASSWD}@{HOST}:{PORT}/{DB}'
+db = SQLAlchemy(app)
+if db: print('DB cnx works')
 Bootstrap = Bootstrap5(app)
 
-ENGINE = ouvrir_connexion(USER, PASSWD, HOST, DB, PORT)
-if ENGINE: print('DB cnx works')
+# ENGINE = ouvrir_connexion(USER, PASSWD, HOST, DB, PORT)
+
+# if ENGINE: print('DB cnx works')
