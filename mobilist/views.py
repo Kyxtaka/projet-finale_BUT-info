b13ab6c58afece1578737862e755c437ad7ef70b
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2024-11-08 10:33:43 +0100

    merge view

--- mobilist/views.py
+++ mobilist/views.py
@@ -198,6 +198,111 @@ def information():
 def services():
     return render_template("services.html")
 
+# Endpoint pour la page d'ajout de logement
+# utilise la methode POST pour l'envoi de formulaire
+# utilisation du json pour la reponse (standard pour les API)
+# Permet de recuperer les pieces d'un logement
+@app.route("/get_pieces/<int:logement_id>")
+@login_required
+def get_pieces(logement_id):
+    pieces = Piece.query.filter_by(id_logement=logement_id).all()
+    pieces_data = [{"id": piece.get_id_piece(), "name": piece.get_nom_piece()} for piece in pieces]
+    return jsonify({"pieces": pieces_data})
+
+@app.route("/bien/ajout", methods=("GET", "POST",))
+@login_required
+def ajout_bien():
+    form_bien = AjoutBienForm()
+    if form_bien.validate_on_submit():
+        try:
+            print("Logs:", form_logs(form_bien))
+            handle_form_bien(form_bien)
+            return redirect(url_for("accueil_connexion"))
+        except Exception as e:
+            print("error ajout bien")
+            print("Logs:", form_logs(form_bien))
+            return render_template("ajout_bien.html", 
+                               form=form_bien,     
+                               error=True)
+    return render_template("ajout_bien.html", 
+                            form=form_bien, 
+                            error=False)
+
+def handle_form_bien(form_bien: AjoutBienForm):
+    try:
+        session = db.session
+        
+        id_bien = Bien.get_max_id()+1
+        nom_bien = form_bien.nom_bien.data
+        date_achat = form_bien.date_bien.data
+        id_proprio =  form_bien.id_proprio
+        prix = form_bien.prix_bien.data
+        id_piece = form_bien.piece_bien.data
+        id_logement = form_bien.logement.data
+        id_type = form_bien.type_bien.data
+        id_cat = form_bien.categorie_bien.data
+        nouv_bien = Bien(
+            id_bien=id_bien, 
+            nom_bien=nom_bien, 
+            date_achat=date_achat, 
+            prix=prix, 
+            id_proprio=id_proprio, 
+            id_piece=id_piece, 
+            id_logement=id_logement, 
+            id_type=id_type, 
+            id_cat=id_cat)
+        session.add(nouv_bien)
+        session.commit()
+        if form_bien.file.data:
+            file_path = form_bien.create_justificatif_bien()
+            print("file path:", file_path)
+            process = link_justification_bien(form_bien, file_path, id_bien)
+            if not process:
+                raise Exception("Erreur lors de l'ajout du justificatif")
+            else:
+                print("Justificatif ajouté")
+    except Exception as e:
+        session.rollback() # afin d eviter les erreurs de commit si une erreur est survenue
+        print("Logs:", form_logs(form_bien)) # affichage des logs
+        print("Exception:", e)
+
+def link_justification_bien(form: AjoutBienForm, file_path: str, id_bien: int) -> bool:
+    session = db.session
+    file = form.file.data
+    id_justificatif = session.query(func.max(Justificatif.id_justif)).scalar() + 1
+    nom_justificatif = file.filename
+    date_ajout = date.today()
+    url = file_path
+    id_bien = id_bien
+    try:
+        new_justificatif = Justificatif(
+            id_justif=id_justificatif,
+            nom_justif=nom_justificatif,
+            date_ajout=date_ajout,
+            URL=url,
+            id_bien=id_bien
+        )
+        session.add(new_justificatif)
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print("Erreur lors de l'ajout du justificatif")
+        print("Exception:", e)
+        return False
+    return True
+
+def form_logs(form: FlaskForm):
+    print("form sumbited:",form.is_submitted())
+    print("form value valid:",form.validate_on_submit())
+    if isinstance(form, AjoutBienForm):
+        print("Ajout bien form detected")
+        if form.file.data:
+            print("file name:", form.file.data.filename)
+            print("file data:", form.file.data)
+        else:
+            print("file data is empty")
+    print("form errors:",form.errors)
+    print("form data:", form.data)
 
 @app.route("/afficheLogements/", methods=("GET", "POST",))
 @login_required   
