f3f0c0f840cbf72a42dbd4cb3f6a92a82b9453c1
Clemence Bocquet <143797406+clemencebc1@users.noreply.github.com>
2024-12-20 11:25:51 +0100

    constante

---
+++ mobilist/secure_constante.py
@@ -0,0 +1,5 @@
+PASSWD = "M@RI@DBD3V" #Ahah bien tenté j'ai modifié le mdp
+GOOGLE_SMTP = "smtp.gmail.com"
+GOOGLE_PORT = 587
+GOOGLE_SMTP_USER = "eexemple044@gmail.com"
+GOOGLE_SMTP_PWD = "ggzb gucf uynu djih"
